55254e0a9304ba48685bcc6161cf30374c2b213a
steffen <steffenb198@aol.com>
2012-01-20 22:20:33 +0100

    Added missing command

---
+++ src/com/iBank/Commands/CommandManager.java
@@ -0,0 +1,67 @@
+package com.iBank.Commands;
+
+import org.bukkit.command.CommandSender;
+
+import com.iBank.system.Bank;
+import com.iBank.system.BankAccount;
+import com.iBank.system.Configuration;
+import com.iBank.system.Handler;
+import com.iBank.system.MessageManager;
+
+/**
+ *  /bank account <NAME> - Shows info about this account
+ *  /bank account <NAME> on 10 - Sets the online percentage of this account to zero
+ *  /bank account <NAME> off 10 - Sets the offline percentage of this account to zero 
+ * @author steffengy
+ *
+ */
+public class CommandManager extends Handler {
+	public void handle(CommandSender sender, String[] arguments) {
+		// General info about account
+		if(arguments.length == 1) {
+			if(Bank.hasAccount(arguments[0])) {
+				BankAccount tmp = Bank.getAccount(arguments[0]);
+				MessageManager.send(sender, "&w&"+Configuration.StringEntry.GeneralInfo.toString().replace("$type$","Account").replace("$name$", arguments[0]));
+				String onlineP = tmp.onDefault ? " Default " : String.valueOf(tmp.getOnlinePercentage()) + "%";
+				String offlinP = tmp.offDefault ? " Default ": String.valueOf(tmp.getOfflinePercentage()) + "%";
+				MessageManager.send(sender, "&w&Online %: &gray&" + onlineP, "");
+				MessageManager.send(sender, "&w&Offline %: &gray&" + offlinP, "");
+			}else{
+				MessageManager.send(sender, "&r&"+Configuration.StringEntry.ErrorNotExist.toString().replace("$name$", "Account "+arguments[0]+" "));
+				return;
+			}
+		// modify percentages and settings
+		}else if(arguments.length == 3) {
+			if(Bank.hasAccount(arguments[0])) {
+				if(arguments[1].equalsIgnoreCase("online") || arguments[1].equalsIgnoreCase("on")) {
+					Double percentage = 0.00;
+					try{
+						percentage = Double.parseDouble(arguments[2]);
+					}catch(Exception e) {
+						MessageManager.send(sender, "&r&"+Configuration.StringEntry.ErrorWrongArguments.toString()+" "+arguments[2]);
+						return;
+					}
+					Bank.getAccount(arguments[0]).setOnPercentage(percentage, true);
+				}else if(arguments[1].equalsIgnoreCase("offline") || arguments[1].equalsIgnoreCase("off")) {
+					Double percentage = 0.00;
+					try{
+						percentage = Double.parseDouble(arguments[2]);
+					}catch(Exception e) {
+						MessageManager.send(sender, "&r&"+Configuration.StringEntry.ErrorWrongArguments.toString()+" "+arguments[2]);
+						return;
+					}
+					Bank.getAccount(arguments[0]).setOffPercentage(percentage, true);
+				}else{
+					MessageManager.send(sender, "&r&"+Configuration.StringEntry.ErrorWrongArguments.toString()+" "+arguments[1]);
+					return;
+				}
+				MessageManager.send(sender, "&g&"+Configuration.StringEntry.SuccessAccount.toString().replace("$name$", arguments[0]));
+			}else{
+				MessageManager.send(sender, "&r&"+Configuration.StringEntry.ErrorNotExist.toString().replace("$name$", "Account "+arguments[0]+" "));
+				return;
+			}
+		} else {
+			MessageManager.send(sender, "&r&"+Configuration.StringEntry.ErrorWrongArguments.toString());
+		}
+	}
+}
