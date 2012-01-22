635900a6055fbac367b5385154a81e674f024870
steffen <steffenb198@aol.com>
2012-01-22 15:44:43 +0100

    Added fee to Open Command

--- src/com/iBank/Commands/CommandOpenAccount.java
+++ src/com/iBank/Commands/CommandOpenAccount.java
@@ -1,5 +1,7 @@
 package com.iBank.Commands;
 
+import java.math.BigDecimal;
+
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -29,6 +31,26 @@ public class CommandOpenAccount extends Handler {
 				return;
 			}
 			if(!Bank.hasAccount(arguments[0])) {
+				// fee stuff
+				String fee = Configuration.Entry.FeeCreate.toString();
+				BigDecimal extra = new BigDecimal("0.00");
+				if(!fee.contains(";")) {
+					extra = iBank.parseFee(fee, new BigDecimal(iBank.economy.getBalance(((Player)sender).getName())));
+				}else{
+					String[] costs = fee.split(";");
+					int account = Bank.getAccountsByOwner(((Player)sender).getName()).size() + 1;
+				
+					if(costs.length > account) {
+						extra = iBank.parseFee(costs[account], new BigDecimal(iBank.economy.getBalance(((Player)sender).getName())));
+					}else{
+						extra = iBank.parseFee(costs[costs.length - 1], new BigDecimal(iBank.economy.getBalance(((Player)sender).getName())));
+					}
+				}
+				if(!iBank.economy.has(((Player)sender).getName(), extra.doubleValue())) {
+					MessageManager.send(sender, "&r&"+Configuration.StringEntry.ErrorNotEnough.toString());
+					return;
+				}
+				iBank.economy.withdrawPlayer(((Player)sender).getName(), extra.doubleValue());
 				Bank.createAccount(arguments[0], ((Player)sender).getName());
 				// check for custom percentages
 				if(region != " ") {
@@ -37,6 +59,7 @@ public class CommandOpenAccount extends Handler {
 					if(!reg.offDefault) Bank.getAccount(arguments[0]).setOffPercentage(reg.getOffPercentage(), true);
 				}
 				MessageManager.send(sender, "&g&"+Configuration.StringEntry.SuccessAddAccount.toString().replace("$name$", "Account "+arguments[0]+" "));
+				if(extra.compareTo(new BigDecimal("0.00"))>0) MessageManager.send(sender, "&g&"+Configuration.StringEntry.PaidFee.toString().replace("$amount$", iBank.format(extra)));
 			}else{
 				MessageManager.send(sender, "&r&"+Configuration.StringEntry.ErrorAlreadyExists.toString().replace("$name$", "Account "+arguments[0]+" "));
 			}
